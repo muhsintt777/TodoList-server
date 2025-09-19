c3f64171864a442fa1843c66d3af81df871b91df
Muhsin <muhsintt777@gmail.com>
2025-09-19 10:30:34 +0530

    feat: add timestamp and defailt value

--- src/api/todo/model.js
+++ src/api/todo/model.js
@@ -1,8 +1,16 @@
 import { model, Schema } from "mongoose";
 
-const todoSchema = new Schema({
-  text: String,
-  isDone: Boolean,
-});
+const todoSchema = new Schema(
+  {
+    text: String,
+    isDone: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export const TodoModel = model("Todo", todoSchema);
