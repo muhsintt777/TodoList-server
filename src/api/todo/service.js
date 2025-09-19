00550909b35186b56b47532b3e102c6d510032f1
Muhsin <muhsintt777@gmail.com>
2025-09-19 10:30:42 +0530

    feat :add validation

--- src/api/todo/service.js
+++ src/api/todo/service.js
@@ -1,21 +1,36 @@
+import { isValidObjectId } from "mongoose";
 import { TodoModel } from "./model.js";
 
 export class TodoService {
   static async getAllTodo() {
-    const todos = await TodoModel.find().exec();
+    const todos = await TodoModel.find().limit(20).lean();
+    if (!todos.length) return [];
     return todos;
   }
 
-  static async addTodo(data) {
-    const newData = new TodoModel(data);
-    return newData.save();
+  static async addTodo(text) {
+    if (typeof text !== "string") {
+      throw new Error("Invalid text");
+    }
+
+    const todo = await TodoModel.create({ text });
+    return todo.id;
   }
 
   static async deleteTodo(id) {
-    return TodoModel.deleteOne({ _id: id }).exec();
+    if (!isValidObjectId(id)) throw new Error("Invalid ID");
+    await TodoModel.deleteOne({ _id: id });
   }
 
-  static async updateTodo(id, data) {
-    return TodoModel.updateOne({ _id: id }, data).exec();
+  static async updateTodo(id, todoDetails) {
+    if (!isValidObjectId(id)) throw new Error("Invalid ID");
+    if (
+      typeof todoDetails?.text !== "string" ||
+      typeof todoDetails?.isDone !== "boolean"
+    ) {
+      throw new Error("Invalid details");
+    }
+
+    await TodoModel.updateOne({ _id: id }, todoDetails);
   }
 }
